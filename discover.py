091de5f6fe1175260398d55afb73865aae36ff5b
Team 1 <morgan.pfister@hotmail.fr>
2017-03-28 15:30:35 +0200

    print mac addresses as an hexadecimal string

--- discover.py
+++ discover.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python
+from nrf import bridge
 
-import nrf
-
-nrf=nrf.bridge()
-ebugs=nrf.assign_addresses()
+nrf = bridge()
+ebugs = nrf.assign_addresses()
 
 for i,j in ebugs.items():
     print i,j
     nrf.set_TX_address(i)
-    nrf.print_bot(repr(j))
+    nrf.print_bot(':'.join([hex(decimal).strip('0x') for decimal in j]))
