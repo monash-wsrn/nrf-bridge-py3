0f7fb2bfa7961cd921c186a6a5a3abe940b0c574
Tony Grubman <tony.grubman@monash.edu>
2017-04-10 12:02:05 +0200

    Fix some python3 translation issues Make get_robot_position independant

--- clustering.py
+++ clustering.py
@@ -4,23 +4,19 @@ import scipy.sparse.csgraph
 
 def Clusters(LEDs, indices, colors, blob_sizes):
     dist_sq = np.array([])
-
     for neigh in indices:
         dist_sq = np.append(dist_sq, ((LEDs[neigh[0]] - LEDs[neigh[1]]) ** 2).sum())
         dist_sq = np.append(dist_sq, ((LEDs[neigh[0]] - LEDs[neigh[2]]) ** 2).sum())
     mid = np.median(dist_sq)
 
     LED_graph = np.zeros((len(LEDs), len(LEDs)))
-
     for i in range(len(dist_sq)):
-        index = i / 2
+        index = i // 2
         neigh_index = i % 2 + 1
         if dist_sq[i] < mid * 2 and dist_sq[i] > mid * 0.5:
             LED_graph[indices[index][0]][indices[index][neigh_index]] = 1
     labels = scipy.sparse.csgraph.connected_components(LED_graph)
-
     cluster_nums = [i for i in range(labels[0])]
-
     valid_cluster = [i for i in cluster_nums if labels[1].tolist().count(i) > 5]
     valid_LEDs = np.array([]).reshape(0, 2)
     valid_cluster_index = np.array([])
@@ -34,5 +30,4 @@ def Clusters(LEDs, indices, colors, blob_sizes):
         valid_cluster_index = np.append(valid_cluster_index, i)
         valid_color = np.hstack((valid_color, color))
         valid_blob = np.hstack((valid_blob, blob_size))
-
     return valid_LEDs, valid_color, valid_blob, valid_cluster_index
